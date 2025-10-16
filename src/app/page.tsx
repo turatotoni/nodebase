8d89b99adffb699b6f29fd856f273f7f81c6ecd2
turatotoni <turatotoni54@gmail.com>
2025-10-16 17:37:27 +0200

    Implemented tRCP for type safety.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,20 @@
-import prisma from "@/lib/db";
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Client } from "./client";
+import { Suspense } from "react";
 
 const Page = async () => {
-  const users = await prisma.user.findMany();
+  const queryClient = getQueryClient();
+
+  void queryClient.prefetchQuery(trpc.getUsers.queryOptions());
 
   return (
     <div className="min-h-screen min-w-screen flex items-center justify-center">
-      {JSON.stringify(users)}
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <Suspense fallback={<p>loading...</p>}>
+          <Client />         
+        </Suspense>
+      </HydrationBoundary>
     </div>
   );
 };
