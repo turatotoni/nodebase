429b3b0bb86e2d128efd173e16cf2f95205d7364
turatotoni <turatotoni54@gmail.com>
2025-11-06 18:25:02 +0100

    AI testing

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,25 +1,33 @@
 import prisma from "@/lib/db";
 import { inngest } from "./client";
+import { createGoogleGenerativeAI } from "@ai-sdk/google";
+import { createOpenAI } from "@ai-sdk/openai";
+import { generateText } from "ai";
 
-export const helloWorld = inngest.createFunction(
-  { id: "hello-world" },
-  { event: "test/hello.world" },
+const google = createGoogleGenerativeAI();
+const openai = createOpenAI();
+
+export const execute = inngest.createFunction(
+  { id: "execute-ai" },
+  { event: "execute/ai" },
   async ({ event, step }) => {
-    //fetching yt video
-    await step.sleep("wait-a-moment", "5s");
+    await step.sleep("pretend", "5s");
 
-    //transcribing
-    await step.sleep("wait-a-moment", "5s");
-
-    //openai
-    await step.sleep("wait-a-moment", "5s");
-
-    await step.run("create-workflow", () => {
-        return prisma.workflow.create({
-            data: {
-                name: "workflow-from-ingest",
-            },
-        });
+    const { steps: geminiSteps } = await step.ai.wrap("gemini-generate-text", generateText, {
+      model: google("gemini-2.5-flash"),
+      system: "You are a helpful assistant.",
+      prompt: "What is 2 + 2?",
     });
+
+    const { steps: openaiSteps } = await step.ai.wrap("openai-generate-text", generateText, {
+      model: openai("gpt-4"),
+      system: "You are a helpful assistant.",
+      prompt: "What is 2 + 2?",
+    });
+    
+    return {
+      geminiSteps,
+      openaiSteps,
+    }
   },
 );
