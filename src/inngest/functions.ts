d8acb5351de648910146c23ef7ffe59eeeb48f1f
turatotoni <turatotoni54@gmail.com>
2025-11-07 00:11:20 +0100

    Setup error tracking with Sentry

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,5 +1,6 @@
 import prisma from "@/lib/db";
 import { inngest } from "./client";
+import * as Sentry from "@sentry/nextjs";
 import { createGoogleGenerativeAI } from "@ai-sdk/google";
 import { createOpenAI } from "@ai-sdk/openai";
 import { generateText } from "ai";
@@ -13,16 +14,30 @@ export const execute = inngest.createFunction(
   async ({ event, step }) => {
     await step.sleep("pretend", "5s");
 
+    Sentry.logger.info('User triggered test log', { log_source: 'sentry_test' })
+    console.warn("Something is missing");
+    console.error("this is a error i want to track");
+
     const { steps: geminiSteps } = await step.ai.wrap("gemini-generate-text", generateText, {
       model: google("gemini-2.5-flash"),
       system: "You are a helpful assistant.",
       prompt: "What is 2 + 2?",
+      experimental_telemetry: {
+        isEnabled: true,
+        recordInputs: true,
+        recordOutputs: true,
+      },
     });
 
     const { steps: openaiSteps } = await step.ai.wrap("openai-generate-text", generateText, {
       model: openai("gpt-4"),
       system: "You are a helpful assistant.",
       prompt: "What is 2 + 2?",
+      experimental_telemetry: {
+        isEnabled: true,
+        recordInputs: true,
+        recordOutputs: true,
+      },
     });
     
     return {
