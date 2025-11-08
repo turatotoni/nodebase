6165b336c2f08ed3a33aa661230ed5694f4ece72
turatotoni <turatotoni54@gmail.com>
2025-11-08 17:49:07 +0100

    Created sidebar layout

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,15 @@
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar"
+import { AppSidebar } from "@/components/app-sidebar";
+
+
+const Layout = ({ children }: { children: React.ReactNode; }) => {
+    return (
+        <SidebarProvider>
+            <AppSidebar />
+            <SidebarInset className="bg-accent/20">
+                {children}
+            </SidebarInset>
+        </SidebarProvider>
+    );
+};
+export default Layout;
